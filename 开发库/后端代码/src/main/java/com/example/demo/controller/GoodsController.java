300a6d41a619aa981e1c991546eadb7ad6ed9413
wwhxh <71277515+wwhxh@users.noreply.github.com>
2021-11-05 10:31:50 +0800

    Add files via upload

--- 开发库/后端代码/src/main/java/com/example/demo/controller/GoodsController.java
+++ 开发库/后端代码/src/main/java/com/example/demo/controller/GoodsController.java
@@ -15,140 +15,246 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.Entity.Goods;
 import com.example.demo.Service.GoodsService;
-import com.example.demo.Service.carService;
-import com.example.demo.Service.userService;
+import com.example.demo.Service.CarService;
+import com.example.demo.Service.UserService;
 
 @CrossOrigin
 @RestController
 public class GoodsController {
-	@Autowired //自动连接到UserService Bean
-    private GoodsService g;
+	@Autowired // 自动连接到UserService Bean
+	private GoodsService goodsService;
 	@Autowired
-	private userService u;
+	private UserService userService;
 	@Autowired
-	private carService c;
-	
+	private CarService carService;
+
+	/**
+	 * 商家上传并提供管理员审核的商品,未注册或非商家上传失败
+	 * 
+	 * @param file      图片
+	 * @param userId	用户id
+	 * @param password	密码
+	 * @param name      姓名
+	 * @param price     价格
+	 * @param discount	折扣
+	 * @param intro     商品简介
+	 * @param newOrOld  新旧程度
+	 * @param catalogue 分类
+	 * @param size      尺寸
+	 * @param bargain   是否可以议价
+	 * @param amount    上架商品的数量
+	 * @return 是否成功上传商品
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 */
 	@CrossOrigin
-	@RequestMapping(value = "/addgood",method = RequestMethod.POST)
-	public String addgood(@RequestParam(value="file",required=false) MultipartFile[] file,String id,String word,
-			String name,String price,String zhekou,String intro,String newo,String fenlei,String size,
-			String yijia,String amount) throws IllegalStateException, IOException {   //商家上架新商品  
-		System.out.println(id);
-		System.out.println(amount);
-		if(u.get(id, word).size() < 1) {
+	@RequestMapping(value = "/addgood", method = RequestMethod.POST)
+	public String addGood(@RequestParam(value = "file", required = false) MultipartFile[] file, String userId, String password,
+			String name, String price, String discount, String intro, String newOrOld, String catalogue, String size,
+			String bargain, String amount) throws IllegalStateException, IOException { // 商家上架新商品
+		// System.out.println(id);
+		// System.out.println(amount);
+		if (userService.getUser(userId, password).size() < 1) {
 			return "fail";
-		}else if(!u.get(id, word).get(0).getStatus().equals("3"))   {
+		} else if (!userService.getUser(userId, password).get(0).getStatus().equals("3")) {
 			return "fail";
 		}
-		if(file == null  ) {
+		if (file == null) {
 			return "请上传商品图片";
 		}
-		String goodid = UUID.randomUUID().toString();
+		String goodId = UUID.randomUUID().toString();
 		String imgUrl = "";
-		String allname = "";
-		String imgName = ""; 
-		for(int i = 0; i < file.length; i++) {
-			allname = file[i].getOriginalFilename();   
-			imgName = goodid+String.valueOf(i)+allname.substring(allname.lastIndexOf("."));
-			file[i].transferTo(new File("C:\\Users\\13049\\Pictures\\good\\"+imgName));
+		String allName = "";
+		String imgName = "";
+		for (int i = 0; i < file.length; i++) {
+			allName = file[i].getOriginalFilename();
+			imgName = goodId + String.valueOf(i) + allName.substring(allName.lastIndexOf("."));
+			file[i].transferTo(new File("C:\\Users\\13049\\Pictures\\good\\" + imgName));
 			imgUrl += imgName + ",";
 		}
-		g.addgood(id, goodid, name, price, zhekou, intro, newo, fenlei, size, yijia, amount, imgUrl);
+		goodsService.addgood(userId, goodId, name, price, discount, intro, newOrOld, catalogue, size, bargain, amount, imgUrl);
 		return "请等待管理员审核后，方可正式上架！";
 	}
-	
+
+	/**
+	 * 管理员获取未审核的商品
+	 * 
+	 * @param userId	用户ID
+	 * @param password	用户密码
+	 * @return 待审核列表
+	 */
 	@CrossOrigin
-	@RequestMapping(value = "/getgood",method = RequestMethod.POST)
-	public List<Goods> getgood(String id,String word) {    //管理员获取待审核的商品
-		if(u.get(id, word).size()  > 0 && u.get(id, word).get(0).getStatus().equals("0")) {
-			return g.getgood();
-		}
-		return null;   	
-	}
-	
-	@CrossOrigin
-	@RequestMapping(value = "/allowgood",method = RequestMethod.POST)
-	public void allowgood(String id,String word,String goodid) {      //允许商品上架
-		System.out.println(goodid);
-		if(u.get(id, word).size() == 1 && u.get(id, word).get(0).getStatus().equals("0")) {
-			g.allowgood(goodid,"2");
-		}
-	}
-	
-	@CrossOrigin
-	@RequestMapping(value = "/getAllgood",method = RequestMethod.POST)
-	public List<Goods> getAllgood() {     //获取所有允许上架的商品展示在主页面
-		//System.out.println(g.getAllgood().toString());
-		return g.getAllgood();   	
-	} 
-	
-	@CrossOrigin
-	@RequestMapping(value = "/goodinfo",method = RequestMethod.POST)
-	public List<Goods> goodinfo(String goodid) {     //商品详情页
-		System.out.println(goodid);
-		return g.goodinfo(goodid);   	
-	} 
-	
-	@CrossOrigin
-	@RequestMapping(value = "/shopgood",method = RequestMethod.POST)
-	public List<Goods> shopgood(String id){   //商家的店铺
-		System.out.println(id);
-		return g.shopgood(id);
-	}
-	
-	@RequestMapping(value = "/allgood",method = RequestMethod.POST)
-	public List<Goods>	allgood(String id,String word){   //商家的店铺
-		if(u.get(id, word).size() > 0) {
-			return g.shopgood(id);
+	@RequestMapping(value = "/getgood", method = RequestMethod.POST)
+	public List<Goods> getUnauditedGood(String userId, String password) {
+		if (userService.getUser(userId, password).size() > 0 && userService.getUser(userId, password).get(0).getStatus().equals("0")) {
+			return goodsService.getUnauditedGood();
 		}
 		return null;
 	}
-	
-	@RequestMapping(value = "/xiajia",method = RequestMethod.POST)
-	public void xiajia(String id,String word,String goodid) {      //允许商品上架
-		if(u.get(id, word).size() > 0) {
-			g.allowgood(goodid,"3");
+
+	/**
+	 * 管理员从已下架的商品中上架
+	 * 
+	 * @param userId     用户ID
+	 * @param word   用户密码
+	 * @param goodid 商品ID
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/allowgood", method = RequestMethod.POST)
+	public void adminUpGood(String userId, String word, String goodid) {
+		// System.out.println(goodid);
+		if (userService.getUser(userId, word).size() == 1 && userService.getUser(userId, word).get(0).getStatus().equals("0")) {
+			goodsService.allowgood(goodid, "2");
 		}
 	}
-	
-	@RequestMapping(value = "/shangjia",method = RequestMethod.POST)
-	public void shangjia(String id,String word,String goodid) {      //允许商品上架
-		if(u.get(id, word).size() > 0) {
-			g.allowgood(goodid,"2");
-		}
-	}
-	
-	@RequestMapping(value = "/xiajiagood",method = RequestMethod.POST)
-	public List<Goods> xiajiagood(String id,String word) {      
-		if(u.get(id, word).size() > 0) {
-			return g.getxiajia(id, "3");
+
+	/**
+	 *
+	 * 管理员管理商品
+	 * 
+	 * @param id   用户ID
+	 * @param word 用户密码
+	 * @return 已下架的商品列表
+	 */
+	@RequestMapping(value = "/xiajiagood", method = RequestMethod.POST)
+	public List<Goods> adminDownGood(String id, String word) {
+		if (userService.getUser(id, word).size() > 0) {
+			return goodsService.getRemovedGoods(id, "3");
 		}
 		return null;
 	}
-	
-	@RequestMapping(value = "/changeamount",method = RequestMethod.POST)
-	public void changeamount(String id,String word,String goodid,String amount){
-		if(u.get(id, word).size() > 0) {
-			g.tui(id, goodid, amount);
-			c.changeamount(id, goodid, amount);
+
+	/**
+	 * 商家上架商品
+	 * 
+	 * @param id     用户ID
+	 * @param word   用户密码
+	 * @param goodId 商品ID
+	 */
+
+	@RequestMapping(value = "/shangjia", method = RequestMethod.POST)
+	public void upGood(String id, String word, String goodId) {
+		if (userService.getUser(id, word).size() > 0) {
+			goodsService.allowgood(goodId, "2");
 		}
 	}
-	
-	@RequestMapping(value = "/changeprice",method = RequestMethod.POST)
-	public void changeprice(String id,String word,String goodid,String price,String p){
-		System.out.println(p);
-		if(u.get(id, word).size() > 0) {
-			g.changeprice(goodid, price);
-			c.changeprice(id, goodid, p);
+
+	/**
+	 * 商家下架商品
+	 * 
+	 * @param id     用户ID
+	 * @param word   用户密码
+	 * @param goodId 商品ID
+	 */
+	@RequestMapping(value = "/xiajia", method = RequestMethod.POST)
+	public void downGood(String id, String word, String goodId) {
+		if (userService.getUser(id, word).size() > 0) {
+			goodsService.allowgood(goodId, "3");
 		}
 	}
-	
-	@RequestMapping(value = "/changezhekou",method = RequestMethod.POST)
-	public void changezhekou(String id,String word,String goodid,String zhekou,String p){
-		if(u.get(id, word).size() > 0) {
-			g.changezhekou(goodid, zhekou);
-			c.changeprice(id, goodid, p);
+
+	/**
+	 * 获取所有允许上架的商品展示在主页面
+	 * 
+	 * @return 上架的商品列表
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/getAllgood", method = RequestMethod.POST)
+	public List<Goods> getAllGood() {
+		// System.out.println(g.getAllgood().toString());
+		return goodsService.getAllgood();
+	}
+
+	/**
+	 * 商品详情页信息
+	 * 
+	 * @param goodid 商品ID
+	 * @return 商品详情信息
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/goodinfo", method = RequestMethod.POST)
+	public List<Goods> goodInfomation(String goodid) {
+		// System.out.println(goodid);
+		return goodsService.goodinfo(goodid);
+	}
+
+	/**
+	 * 获取店铺商品
+	 * 
+	 * @param id 用户ID
+	 * @return 店铺信息
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/shopgood", method = RequestMethod.POST)
+	public List<Goods> shopInformation(String id) {
+		// System.out.println(id);
+		return goodsService.shopgood(id);
+	}
+
+	/**
+	 * 获取所有商品
+	 * 
+	 * @param id
+	 * @param word
+	 * @return
+	 */
+	@RequestMapping(value = "/allgood", method = RequestMethod.POST)
+	public List<Goods> shopGood(String id, String word) {
+		if (userService.getUser(id, word).size() > 0) {
+			return goodsService.shopgood(id);
+		}
+		return null;
+	}
+
+	/**
+	 * 修改商品数量
+	 * 
+	 * @param id     用户ID
+	 * @param word   用户密码
+	 * @param goodid 商品ID
+	 * @param amount 商品数量
+	 */
+	@RequestMapping(value = "/changeamount", method = RequestMethod.POST)
+	public void changeAmount(String id, String word, String goodid, String amount) {
+		if (userService.getUser(id, word).size() > 0) {
+			goodsService.changeAmount(id, goodid, amount);
+			carService.changeamount(id, goodid, amount);
 		}
 	}
-	
+
+	/**
+	 * 修改商品价格
+	 * 
+	 * @param id       用户ID
+	 * @param word     用户密码
+	 * @param goodid   商品ID
+	 * @param price    商品旧价格
+	 * @param newprice 商品新价格
+	 */
+	@RequestMapping(value = "/changeprice", method = RequestMethod.POST)
+	public void changePrice(String id, String word, String goodid, String price, String newprice) {
+		//System.out.println(newprice);
+		if (userService.getUser(id, word).size() > 0) {
+			goodsService.changeprice(goodid, price);
+			carService.changeprice(id, goodid, newprice);
+		}
+	}
+
+	/**
+	 * 修改商品折扣
+	 * 
+	 * @param id       用户ID
+	 * @param word     用户密码
+	 * @param goodId   商品ID
+	 * @param discount 商品折扣
+	 * @param price    商品价格
+	 */
+	@RequestMapping(value = "/changezhekou", method = RequestMethod.POST)
+	public void changeDiscount(String id, String word, String goodId, String discount, String price) {
+		if (userService.getUser(id, word).size() > 0) {
+			goodsService.changeDisacount(goodId, discount);
+			carService.changeprice(id, goodId, price);
+		}
+	}
+
 }
--- 开发库/后端代码/src/main/java/com/example/demo/controller/UserController.java
+++ 开发库/后端代码/src/main/java/com/example/demo/controller/UserController.java
@@ -18,147 +18,272 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.Entity.User;
-import com.example.demo.Service.userService;
+import com.example.demo.Service.UserService;
 
 @CrossOrigin
 @RestController
-public class UserController {    //用户控制器
-	@Autowired //自动连接到UserService Bean
-    private userService userS;
-	
+public class UserController { // 用户控制器
+	@Autowired // 自动连接到UserService Bean
+	private UserService userService;
+
+	/**
+	 * 输出Hello World!
+	 * 
+	 * @return String
+	 */
 	@RequestMapping(value = "/show")
 	public String show() {
-		return userS.show();
+		return userService.show();
 	}
-	
-	            //登录验证接口
-	@RequestMapping(value = "/login",method = RequestMethod.POST)
-	public  String login(String id, String word) {
-		System.out.println(id);
-		System.out.println(word);
-		if(userS.get(id, word).size() < 1 || userS.get(id, word) == null) {
+
+	/**
+	 * 登录验证
+	 * 
+	 * @param id   用户ID
+	 * @param word 用户密码
+	 * @return parameter登录角色
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(String id, String word) {
+		// System.out.println(id);
+		// System.out.println(word);
+		if (userService.getUser(id, word).size() < 1 || userService.getUser(id, word) == null) {
 			return "账号或密码错误！";
-		}else if(userS.get(id, word).get(0).getStatus().equals("0")) {
+		} else if (userService.getUser(id, word).get(0).getStatus().equals("0")) {
 			return "admin";
-		}else if(userS.get(id, word).get(0).getStatus().equals("1") || userS.get(id, word).get(0).getStatus().equals("2") ||
-				userS.get(id, word).get(0).getStatus().equals("3")) {
+		} else if (userService.getUser(id, word).get(0).getStatus().equals("1")
+				|| userService.getUser(id, word).get(0).getStatus().equals("2")
+				|| userService.getUser(id, word).get(0).getStatus().equals("3")) {
 			return "user";
 		}
 		return "账号或密码错误！";
 	}
-	
-	@RequestMapping(value = "/login1",method = RequestMethod.POST)
-	public  List<User> login1(String id, String word) {
-		if(userS.get(id, word).size() > 0) {
-			return userS.get(id, word);
+
+	/**
+	 * 验证用户状态
+	 * 
+	 * @param id   用户ID
+	 * @param word 用户密码
+	 * @return 登录是否成功
+	 */
+	@RequestMapping(value = "/login1", method = RequestMethod.POST)
+	public List<User> login1(String id, String word) {
+		if (userService.getUser(id, word).size() > 0) {
+			return userService.getUser(id, word);
 		}
 		return null;
 	}
-	
-	             
-	@RequestMapping(value = "/insertZhuce",method = RequestMethod.POST)
-	public void insertZhuce(String id,String word,String name,String sex,
-			String phone,String email,String city,String account) {    //注册用户信息插入待验证用户表
-		userS.insertZhuce(id, word, name, sex, phone, email, city, account);
+
+	/**
+	 * 注册用户信息插入待验证用户表
+	 * 
+	 * @param id      用户ID
+	 * @param word    用户密码
+	 * @param name    姓名
+	 * @param sex     性别
+	 * @param phone   电话
+	 * @param email   电子邮箱
+	 * @param city    城市
+	 * @param account 银行账户
+	 */
+	@RequestMapping(value = "/insertZhuce", method = RequestMethod.POST)
+	public void insertRegisteredUser(String id, String word, String name, String sex, String phone, String email, String city,
+			String account) {
+		userService.insertRegisteredUser(id, word, name, sex, phone, email, city, account);
 	}
-	
-	@CrossOrigin             //验证注册用户是否重复提交
-	@RequestMapping(value = "/getZhuceUser",method = RequestMethod.POST)
-	public  List<User> getZhuceUser(String id) {
-		return userS.getZhuceUser(id);
+
+	/**
+	 * 验证注册用户是否重复提交
+	 * 
+	 * @param id 用户ID
+	 * @return 已注册未审核的id查询
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/getZhuceUser", method = RequestMethod.POST)
+	public List<User> getRegisteredUser(String id) {
+		return userService.getRegisteredUser(id);
 	}
-	
-	
-	@CrossOrigin             //获取待验证的注册用户列表
-	@RequestMapping(value = "/getUser",method = RequestMethod.POST)
-	public  List<User> getUser(String id,String word) {
-		if(userS.get(id, word).get(0).getStatus().equals("0")) {
-			return userS.getUser();
+
+	/**
+	 * 获取待验证的注册用户列表
+	 * 
+	 * @param id   用户ID
+	 * @param word 用户密码
+	 * @return 待验证列表
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/getUser", method = RequestMethod.POST)
+	public List<User> getUnauditedUser(String id, String word) {
+		if (userService.getUser(id, word).get(0).getStatus().equals("0")) {
+			return userService.getUnauditedUser();
 		}
 		return null;
 	}
-	
-	@CrossOrigin            
-	@RequestMapping(value = "/deleteUser",method = RequestMethod.POST)
-	public void deleteUser(String id) {   //删除待验证表的允许登录的注册用户
-   	 	userS.deleteUser(id);
-   	 	System.out.println(id);
-	}
-	
-	@CrossOrigin            
-	@RequestMapping(value = "/insertUser",method = RequestMethod.POST)
-	public void insertUser(String id,String word,String name,String sex,
-			String phone,String email,String city,String account) {    //允许登录的注册用户信息插入用户表
-		userS.insertUser(id, word, name, sex, phone, email, city, account,"1");
-	}
-	
-	@CrossOrigin            
-	@RequestMapping(value = "/personal",method = RequestMethod.POST)
-	public List<User> getpersonnalinfo(String id,String word) {    //获取用户个人信息
+
+	/**
+	 * 删除待验证表的允许登录的注册用户
+	 * 
+	 * @param id 用户ID
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/deleteUser", method = RequestMethod.POST)
+	public void deleteUser(String id) {
+		userService.deleteUser(id);
 		System.out.println(id);
-		System.out.println(word);
-		return userS.getuserinfo(id, word);
 	}
-	
-	@CrossOrigin            
-	@RequestMapping(value = "/updateInfo",method = RequestMethod.POST)
-	public void updateInfo(String name,String phone,String email,String city,String account,String id,String word) {  //用户更改个人信息
-		userS.updateInfo(name, phone, email, city, account, id, word);
+
+	/**
+	 * 审核通过的用户插入允许登录表
+	 * 
+	 * @param id      用户ID
+	 * @param word    用户密码
+	 * @param name    姓名
+	 * @param sex     性别
+	 * @param phone   电话号码
+	 * @param email   电子邮箱
+	 * @param city    城市
+	 * @param account 银行账户
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/insertUser", method = RequestMethod.POST)
+	public void insertUser(String id, String word, String name, String sex, String phone, String email, String city,
+			String account) {
+		userService.insertUser(id, word, name, sex, phone, email, city, account, "1");
 	}
-	
-	@CrossOrigin            
-	@RequestMapping(value = "/shopzhuce",method = RequestMethod.POST)
-	public String shopzhuce(@RequestParam(value="licence",required=false) MultipartFile licence,
-			@RequestParam(value="card",required=false) MultipartFile card,String id,String word) throws IllegalStateException, IOException {    //用户注册商家
+
+	/**
+	 * 获取用户个人信息
+	 * 
+	 * @param id   用户ID
+	 * @param word 用户密码
+	 * @return List 用户信息
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/personal", method = RequestMethod.POST)
+	public List<User> getUserInfo(String id, String word) {
+		//System.out.println(id);
+		//System.out.println(word);
+		return userService.getUserInfo(id, word);
+	}
+
+	/**
+	 * 修改个人信息
+	 * 
+	 * @param name    姓名
+	 * @param phone   电话号码
+	 * @param email   电子邮箱
+	 * @param city    城市
+	 * @param account 银行账户
+	 * @param id      用户ID
+	 * @param word    用户密码
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/updateInfo", method = RequestMethod.POST)
+	public void updateUserInfo(String name, String phone, String email, String city, String account, String id,
+			String word) { // 用户更改个人信息
+		userService.updateUserInfo(name, phone, email, city, account, id, word);
+	}
+
+	/**
+	 * 用户申请注册成为商家
+	 * 
+	 * @param licence
+	 * @param card
+	 * @param id      用户ID
+	 * @param word    用户密码
+	 * @return
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 */
+	@CrossOrigin
+	@RequestMapping(value = "/shopzhuce", method = RequestMethod.POST)
+	public String shopRegister(@RequestParam(value = "licence", required = false) MultipartFile licence,
+			@RequestParam(value = "card", required = false) MultipartFile card, String id, String word)
+			throws IllegalStateException, IOException { // 用户注册商家
 		System.out.println(id);
-		if(licence == null || card == null) {
+		if (licence == null || card == null) {
 			return "上传失败，请您重新上传！";
 		}
-		String allname = licence.getOriginalFilename();    //营业执照保存至文件夹
-		String licenceName = id+"licence"+allname.substring(allname.lastIndexOf("."));
-		licence.transferTo(new File("C:\\Users\\13049\\Pictures\\shop\\"+licenceName));
-		
-		String allname2 = card.getOriginalFilename();      //身份证照片保存至文件夹
-		String cardName = id+"card"+allname2.substring(allname2.lastIndexOf("."));
-		card.transferTo(new File("C:\\Users\\13049\\Pictures\\shop\\"+cardName));
-		
-		userS.shopzhuce(id, word, licenceName, cardName, "2");
+		String allname = licence.getOriginalFilename(); // 营业执照保存至文件夹
+		String licenceName = id + "licence" + allname.substring(allname.lastIndexOf("."));
+		licence.transferTo(new File("C:\\Users\\13049\\Pictures\\shop\\" + licenceName));
+
+		String allname2 = card.getOriginalFilename(); // 身份证照片保存至文件夹
+		String cardName = id + "card" + allname2.substring(allname2.lastIndexOf("."));
+		card.transferTo(new File("C:\\Users\\13049\\Pictures\\shop\\" + cardName));
+
+		userService.shopRegister(id, word, licenceName, cardName, "2");
 		return "您已申请注册，请等待管理员确认！";
 	}
-	            
-	@RequestMapping(value = "/getshopzhuce",method = RequestMethod.POST)
-	public List<User> getshopzhuce(String id,String word) {    //获取注册商家的用户信息
-		if(this.login(id, word) == "admin") {
-			return userS.getshopzhuce();
+
+	/**
+	 * 获取申请注册商家的用户信息
+	 * 
+	 * @param id   用户ID
+	 * @param word 用户密码
+	 * @return List<User> 申请的用户表
+	 */
+	@RequestMapping(value = "/getshopzhuce", method = RequestMethod.POST)
+	public List<User> getUnauditedShop(String id, String word) {
+		if (this.login(id, word) == "admin") {
+			return userService.getUnauditedShop();
 		}
 		return null;
 	}
-	            
-	@RequestMapping(value = "/allowshopzhuce",method = RequestMethod.POST)
-	public void allowshopzhuce(String id) {  //用户更改个人信息
-		userS.allowshopzhuce(id);;
+
+	/**
+	 * 审核商家列表
+	 * 
+	 * @param id 用户ID
+	 */
+	@RequestMapping(value = "/allowshopzhuce", method = RequestMethod.POST)
+	public void allowShopApply(String id) {
+		userService.allowShopApply(id);
 	}
-	
-	@RequestMapping(value = "/getshoplevel",method = RequestMethod.POST)
-	public List<User> getshoplevel(String id,String word) {  
-		if(userS.get(id, word).size() > 0) {
-			return userS.getshoplevel();
+
+	/**
+	 * 获取商家等级
+	 * 
+	 * @param id   用户ID
+	 * @param word 用户密码
+	 * @return List 商家等级列表
+	 */
+	@RequestMapping(value = "/getshoplevel", method = RequestMethod.POST)
+	public List<User> getShopLevel(String id, String word) {
+		if (userService.getUser(id, word).size() > 0) {
+			return userService.getShopLevel();
 		}
 		return null;
 	}
-	
-	@RequestMapping(value = "/changelevel",method = RequestMethod.POST)
-	public void changelevel(String id,String word,String uid,String level) {  
-		if(userS.get(id, word).size() > 0) {
-			userS.changelevel(uid, level);
+
+	/**
+	 * 管理员管理商家等级
+	 * 
+	 * @param id    用户ID
+	 * @param word  用户密码
+	 * @param uid
+	 * @param level 商家等级
+	 */
+	@RequestMapping(value = "/changelevel", method = RequestMethod.POST)
+	public void changeLevel(String id, String word, String uid, String level) {
+		if (userService.getUser(id, word).size() > 0) {
+			userService.changeLevel(uid, level);
 		}
 	}
-	
-	@RequestMapping(value = "/chongzhi",method = RequestMethod.POST)
-	public void chongzhi(String id,String word,String money,String v) {  
-		if(userS.get(id, word).size() > 0) {
-			double a = Double.parseDouble(money) + Double.parseDouble(v);
-			userS.chongzhi(id, String.valueOf(a));
+
+	/**
+	 * 充值钱包
+	 * 
+	 * @param id     用户ID
+	 * @param word   用户密码
+	 * @param money  账户余额
+	 * @param charge 充值金额
+	 */
+	@RequestMapping(value = "/chongzhi", method = RequestMethod.POST)
+	public void rechargeWallet(String id, String word, String money, String charge) {
+		if (userService.getUser(id, word).size() > 0) {
+			double a = Double.parseDouble(money) + Double.parseDouble(charge);
+			userService.rechargeWallet(id, String.valueOf(a));
 		}
 	}
 }
